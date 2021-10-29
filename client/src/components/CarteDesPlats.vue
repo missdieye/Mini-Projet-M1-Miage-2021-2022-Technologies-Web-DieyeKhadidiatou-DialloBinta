ef060bffee23717408b729912626fe118a8ef89c
Binta55 <71668834+Binta55@users.noreply.github.com>
2021-10-29 19:59:29 +0200

    update

--- client/src/components/CarteDesPlats.vue
+++ client/src/components/CarteDesPlats.vue
@@ -1,12 +1,286 @@
 <template>
-  <div class="">
-    <md-toolbar class="md-dense">
+
+<div class="card-expansion">
+    <md-card>
+      <md-card-media>
+        <img src="../assets/photosResto/decor.jpg" alt="People">
+      </md-card-media>
+
+      <md-card-header>
+        <div class="md-title">nom du plat</div>
+        <div class="md-subhead">Prix</div>
+      </md-card-header>
+
+      <md-card-expand>
+        <md-card-actions md-alignment="space-between">
+          <div>
+            <md-button>En savoir plus</md-button>
+          </div>
+
+          <md-card-expand-trigger>
+            <md-button class="md-icon-button">
+              <md-icon>keyboard_arrow_down</md-icon>
+            </md-button>
+          </md-card-expand-trigger>
+        </md-card-actions>
+
+        <md-card-expand-content>
+          <md-card-content>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+          </md-card-content>
+        </md-card-expand-content>
+      </md-card-expand>
+    </md-card>
+
+    <md-card>
+      <md-card-media>
+        <img src="../assets/photosResto/decor.jpg" alt="People">
+      </md-card-media>
+
+      <md-card-header>
+        <div class="md-title">nom du plat</div>
+        <div class="md-subhead">Prix</div>
+      </md-card-header>
+
+      <md-card-expand>
+        <md-card-actions md-alignment="space-between">
+          <div>
+            <md-button>En savoir plus</md-button>
+          </div>
+
+          <md-card-expand-trigger>
+            <md-button class="md-icon-button">
+              <md-icon>keyboard_arrow_down</md-icon>
+            </md-button>
+          </md-card-expand-trigger>
+        </md-card-actions>
+
+        <md-card-expand-content>
+          <md-card-content>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+          </md-card-content>
+        </md-card-expand-content>
+      </md-card-expand>
+    </md-card>
+     <md-card>
+      <md-card-media>
+        <img src="../assets/photosResto/decor.jpg" alt="People">
+      </md-card-media>
+
+      <md-card-header>
+        <div class="md-title">nom du plat</div>
+        <div class="md-subhead">Prix</div>
+      </md-card-header>
+
+      <md-card-expand>
+        <md-card-actions md-alignment="space-between">
+          <div>
+            <md-button>En savoir plus</md-button>
+          </div>
+
+          <md-card-expand-trigger>
+            <md-button class="md-icon-button">
+              <md-icon>keyboard_arrow_down</md-icon>
+            </md-button>
+          </md-card-expand-trigger>
+        </md-card-actions>
+
+        <md-card-expand-content>
+          <md-card-content>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+          </md-card-content>
+        </md-card-expand-content>
+      </md-card-expand>
+    </md-card>
+     <md-card>
+      <md-card-media>
+        <img src="../assets/photosResto/decor.jpg" alt="People">
+      </md-card-media>
+
+      <md-card-header>
+        <div class="md-title">nom du plat</div>
+        <div class="md-subhead">Prix</div>
+      </md-card-header>
+
+      <md-card-expand>
+        <md-card-actions md-alignment="space-between">
+          <div>
+            <md-button>En savoir plus</md-button>
+          </div>
+
+          <md-card-expand-trigger>
+            <md-button class="md-icon-button">
+              <md-icon>keyboard_arrow_down</md-icon>
+            </md-button>
+          </md-card-expand-trigger>
+        </md-card-actions>
+
+        <md-card-expand-content>
+          <md-card-content>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+          </md-card-content>
+        </md-card-expand-content>
+      </md-card-expand>
+    </md-card>
+     <md-card>
+      <md-card-media>
+        <img src="../assets/photosResto/decor.jpg" alt="People">
+      </md-card-media>
+
+      <md-card-header>
+        <div class="md-title">nom du plat</div>
+        <div class="md-subhead">Prix</div>
+      </md-card-header>
+
+      <md-card-expand>
+        <md-card-actions md-alignment="space-between">
+          <div>
+            <md-button>En savoir plus</md-button>
+          </div>
+
+          <md-card-expand-trigger>
+            <md-button class="md-icon-button">
+              <md-icon>keyboard_arrow_down</md-icon>
+            </md-button>
+          </md-card-expand-trigger>
+        </md-card-actions>
+
+        <md-card-expand-content>
+          <md-card-content>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+          </md-card-content>
+        </md-card-expand-content>
+      </md-card-expand>
+    </md-card>
+     <md-card>
+      <md-card-media>
+        <img src="../assets/photosResto/decor.jpg" alt="People">
+      </md-card-media>
+
+      <md-card-header>
+        <div class="md-title">nom du plat</div>
+        <div class="md-subhead">Prix</div>
+      </md-card-header>
+
+      <md-card-expand>
+        <md-card-actions md-alignment="space-between">
+          <div>
+            <md-button>En savoir plus</md-button>
+          </div>
+
+          <md-card-expand-trigger>
+            <md-button class="md-icon-button">
+              <md-icon>keyboard_arrow_down</md-icon>
+            </md-button>
+          </md-card-expand-trigger>
+        </md-card-actions>
+
+        <md-card-expand-content>
+          <md-card-content>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+          </md-card-content>
+        </md-card-expand-content>
+      </md-card-expand>
+    </md-card>
+     <md-card>
+      <md-card-media>
+        <img src="../assets/photosResto/decor.jpg" alt="People">
+      </md-card-media>
+
+      <md-card-header>
+        <div class="md-title">nom du plat</div>
+        <div class="md-subhead">Prix</div>
+      </md-card-header>
+
+      <md-card-expand>
+        <md-card-actions md-alignment="space-between">
+          <div>
+            <md-button>En savoir plus</md-button>
+          </div>
+
+          <md-card-expand-trigger>
+            <md-button class="md-icon-button">
+              <md-icon>keyboard_arrow_down</md-icon>
+            </md-button>
+          </md-card-expand-trigger>
+        </md-card-actions>
+
+        <md-card-expand-content>
+          <md-card-content>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+          </md-card-content>
+        </md-card-expand-content>
+      </md-card-expand>
+    </md-card>
+     <md-card>
+      <md-card-media>
+        <img src="../assets/photosResto/decor.jpg" alt="People">
+      </md-card-media>
+
+      <md-card-header>
+        <div class="md-title">nom du plat</div>
+        <div class="md-subhead">Prix</div>
+      </md-card-header>
+
+      <md-card-expand>
+        <md-card-actions md-alignment="space-between">
+          <div>
+            <md-button>En savoir plus</md-button>
+          </div>
+
+          <md-card-expand-trigger>
+            <md-button class="md-icon-button">
+              <md-icon>keyboard_arrow_down</md-icon>
+            </md-button>
+          </md-card-expand-trigger>
+        </md-card-actions>
+
+        <md-card-expand-content>
+          <md-card-content>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+          </md-card-content>
+        </md-card-expand-content>
+      </md-card-expand>
+    </md-card>
+     <md-card>
+      <md-card-media>
+        <img src="../assets/photosResto/decor.jpg" alt="People">
+      </md-card-media>
+
+      <md-card-header>
+        <div class="md-title">nom du plat</div>
+        <div class="md-subhead">Prix</div>
+      </md-card-header>
+
+      <md-card-expand>
+        <md-card-actions md-alignment="space-between">
+          <div>
+            <md-button>En savoir plus</md-button>
+          </div>
+
+          <md-card-expand-trigger>
+            <md-button class="md-icon-button">
+              <md-icon>keyboard_arrow_down</md-icon>
+            </md-button>
+          </md-card-expand-trigger>
+        </md-card-actions>
+
+        <md-card-expand-content>
+          <md-card-content>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+          </md-card-content>
+        </md-card-expand-content>
+      </md-card-expand>
+    </md-card>
+  </div>
+
+   <!--  <md-toolbar class="md-dense">
          <md-card>
             <md-card-media>
               <img src="../assets/photosResto/decor.jpg" alt="People">
             </md-card-media>
           </md-card>
-      <h3 class="md-title">nom du plat</h3>
+      <h3 class="md-title">nom du plat ,  prix</h3>
        <div class="info md-toolbar-section-end">
         <md-button class=" buttonInfo md-raised" @click="infoRestoModal=true">
           <md-icon>info</md-icon><span>Informations du Restaurant</span>
@@ -25,10 +299,8 @@
 
             </md-dialog>
       </div>
-    </md-toolbar>
+    </md-toolbar> -->
     
-    
-  </div>
 
 </template>
 
@@ -40,7 +312,7 @@ export default {
   },
   data: function(){
       return{
-        infoRestoModal : false,
+        
       }
 
   }
@@ -49,5 +321,14 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+ .card-expansion {
+    height: 480px;
+  }
 
+  .md-card {
+    width: 320px;
+    margin: 4px;
+    display: inline-block;
+    vertical-align: top;
+  }
 </style>
